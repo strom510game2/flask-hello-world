db1a2fb796aab61decb075bd7c0622f2b42038f4
strom510game2 <strom510game2@gmail.com>
2026-04-30 01:29:24 +0800

    Update app.py 23302

--- app.py
+++ app.py
@@ -9,11 +9,10 @@ stock_no = "2330"
 stock_name = "台積電"
 
 
-# 取得股價（最新收盤價）
+# 取得股價
 def get_stock_price():
     try:
         today = datetime.datetime.now()
-
         date_str = today.strftime("%Y%m01")
 
         url = (
@@ -22,22 +21,19 @@ def get_stock_price():
         )
 
         response = requests.get(url, timeout=10)
-
         data = response.json()
 
         if "data" not in data:
-            print("No data field")
-            return None
-
-        if len(data["data"]) == 0:
-            print("Empty data")
             return None
 
         closes = []
 
         for row in data["data"]:
-            close_price = row[6].replace(",", "")
-            closes.append(float(close_price))
+            price = row[6].replace(",", "")
+            closes.append(float(price))
+
+        if len(closes) == 0:
+            return None
 
         return closes[-1]
 
@@ -46,23 +42,10 @@ def get_stock_price():
         return None
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-# 計算真支撐 / 壓力
- def get_support_resistance():
+# 計算支撐 / 壓力
+def get_support_resistance():
     try:
         today = datetime.datetime.now()
-
         date_str = today.strftime("%Y%m01")
 
         url = (
@@ -71,7 +54,6 @@ def get_stock_price():
         )
 
         response = requests.get(url, timeout=10)
-
         data = response.json()
 
         if "data" not in data:
@@ -80,8 +62,8 @@ def get_stock_price():
         closes = []
 
         for row in data["data"]:
-            close_price = row[6].replace(",", "")
-            closes.append(float(close_price))
+            price = row[6].replace(",", "")
+            closes.append(float(price))
 
         closes = closes[-60:]
 
@@ -104,7 +86,9 @@ def home():
     price = get_stock_price()
 
     if price is None:
-        return jsonify({"error": "無法取得股價"})
+        return jsonify({
+            "error": "無法取得股價"
+        })
 
     support, resistance = get_support_resistance()
 
